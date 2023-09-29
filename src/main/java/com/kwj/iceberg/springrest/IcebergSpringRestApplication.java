325fc3d01cac6350eb9e6da019ec4c142ababb52
woo-jae-kim <woo_jae.kim@navercorp.com>
2023-09-29 15:55:57 +0900

    init project

---
+++ src/main/java/com/kwj/iceberg/springrest/IcebergSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.kwj.iceberg.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcebergSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcebergSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kwj/iceberg/springrest/servlet/IcebergServletConfig.java
@@ -0,0 +1,43 @@
+package com.kwj.iceberg.springrest.servlet;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.iceberg.CatalogProperties;
+import org.apache.iceberg.catalog.Catalog;
+import org.apache.iceberg.jdbc.JdbcCatalog;
+import org.apache.iceberg.rest.RESTCatalogAdapter;
+import org.apache.iceberg.rest.RESTCatalogServlet;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Files;
+
+@org.springframework.context.annotation.Configuration
+public class IcebergServletConfig {
+
+	@Bean
+	@ConditionalOnMissingBean(Catalog.class)
+	public Catalog memoryCatalog() {
+		Map<String, String> catalogProperties = ImmutableMap.of(
+			CatalogProperties.WAREHOUSE_LOCATION, Files.createTempDir().getAbsolutePath(),
+			CatalogProperties.URI, "jdbc:sqlite:file::memory:?ic" + UUID.randomUUID().toString().replace("-", ""),
+			JdbcCatalog.PROPERTY_PREFIX + "username", "user",
+			JdbcCatalog.PROPERTY_PREFIX + "password", "password"
+		);
+
+		JdbcCatalog memoryCatalog = new JdbcCatalog();
+		memoryCatalog.setConf(new Configuration());
+		memoryCatalog.initialize("memory", catalogProperties);
+		return memoryCatalog;
+	}
+
+	@Bean
+	public ServletRegistrationBean<RESTCatalogServlet> restCatalogServlet(Catalog catalog) {
+		return new ServletRegistrationBean<>(new RESTCatalogServlet(new RESTCatalogAdapter(catalog)), "/");
+	}
+
+}
